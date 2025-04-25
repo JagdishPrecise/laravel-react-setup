aefcc7427c476529661d0f2ba9ca394c1b75b575
Jagdish Chaudhary <122854510+JagdishPrecise@users.noreply.github.com>
2025-04-25 10:09:46 +0530

    [WIP- Google Login

---
+++ app/Http/Controllers/Auth/GoogleController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Google_Client;
+use Google_Service_Oauth2;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+
+
+class GoogleController extends Controller
+{
+    public function redirect()
+    {
+
+        $client     = new Google_Client();
+        $client->setClientId(env('GOOGLE_CLIENT_ID'));
+        $client->setClientSecret(env('GOOGLE_CLIENT_SECRET'));
+        $client->setRedirectUri(env('GOOGLE_SIGNIN_URL'));
+        $client->addScope("email");
+        $client->addScope("profile");
+        $client->setAccessType('offline');
+        $client->setApprovalPrompt('force');
+        $authUrl    = $client->createAuthUrl();
+
+        return redirect()->to($authUrl);
+    }
+
+
+    public function authCallBack(Request $request)
+    {
+
+        try {
+            $client       = new Google_Client();
+            $client->setClientId(env('GOOGLE_CLIENT_ID'));
+            $client->setClientSecret(env('GOOGLE_CLIENT_SECRET'));
+            $client->setRedirectUri(env('GOOGLE_SIGNIN_URL'));
+
+            if ($request->has('code')) {
+                $token          = $client->fetchAccessTokenWithAuthCode($request->code);
+                $client->setAccessToken($token);
+                $google_oauth   = new Google_Service_Oauth2($client);
+                $googleUser     = $google_oauth->userinfo->get();
+
+                dd($googleUser);
+                $user           = User::where('email', $googleUser->email)->first();
+
+                if (!$user) {
+                    $newUser = User::create([
+                        'name'      => $googleUser->name,
+                        'email'     => $googleUser->email,
+                        // 'googleId'  => $googleUser->id
+                    ]);
+
+                    Auth::login($newUser);
+                    return redirect('/dashboard');
+                } elseif ($user->email == $googleUser->email && $user->googleId == '') {
+                    $user->update([
+                        // 'googleId' => $googleUser->id
+                    ]);
+
+                    Auth::login($user);
+                    return redirect('/dashboard');
+                } else {
+                    Auth::login($user);
+                    return redirect('/dashboard');
+                }
+            }
+        } catch (\Exception $e) {
+            return redirect('login');
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,17 @@
 <?php
 
+use App\Http\Controllers\Auth\GoogleController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+
 
 Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
+
+
+Route::get('auth/google', [GoogleController::class, 'redirect'])->name('google-auth');
+Route::get('auth/callback', [GoogleController::class, 'authCallBack']);
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
